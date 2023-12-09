c31b865c860dc16bcdbaf9803dc1040358743b08
saigopin <saigopin@byteridge.com>
2023-12-09 11:05:09 +0530

    added send and get messages

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,22 +1,48 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import {setGlobalOptions} from "firebase-functions/v2/options";
+import admin = require("firebase-admin");
+
+admin.initializeApp();
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 // Set the maximum instances to 10 for all functions
 setGlobalOptions({maxInstances: 10});
 
-export const helloWorld = onRequest((request, response) => {
+exports.helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase from Telugu");
 });
+
+exports.sendMessage = onRequest(async (req, res) => {
+  try {
+    if (req.method === "POST") {
+      const body = req.body;
+      if (!body) {
+        res.status(400).send("Body should not be empty.");
+      }
+      // Add timestamp
+      body.timestamp = admin.firestore.FieldValue.serverTimestamp();
+      await admin.firestore().collection("chats").add(body);
+      res.status(201).send("sent message successfully.");
+    } else {
+      res.status(400).send("Method type should be post.");
+    }
+  } catch (e) {
+    logger.error("sendMessage", e);
+    res.status(500).send("Internal server error.");
+  }
+});
+
+exports.getMessages = onRequest(async (req, res) => {
+  try {
+    const snapshot = await admin.firestore().collection("chats").get();
+    const response = snapshot.docs.map((doc) => doc.data());
+    res.status(200).json({response});
+  } catch (e) {
+    logger.error("getMessages", e);
+    res.status(500).send("Internal server error.");
+  }
+});
+
